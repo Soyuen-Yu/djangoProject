4915e009df3a660e3c439b4f045a8b1dff6b31bd
soyuen <lseka82@gmail.com>
2023-03-16 18:40:27 +0900

    for문 생성

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from accountapp.models import HelloWorld
 
@@ -8,13 +9,15 @@ from accountapp.models import HelloWorld
 
 
 def hello_world(request):
-    if request.method == 'POST' :
+    if request.method == 'POST':
         temp = request.POST.get('hello_world_input')
 
         new_hello_world = HelloWorld()
         new_hello_world.text = temp
         new_hello_world.save()
 
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
-    else :
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD'})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+    else:
+
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
