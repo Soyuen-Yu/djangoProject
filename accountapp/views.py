0a170468828d57e467bd0c93f8c58d93a52cb71b
soyuen <lseka82@gmail.com>
2023-03-22 22:03:01 +0900

    계정인증 만들기

--- accountapp/views.py
+++ accountapp/views.py
@@ -13,18 +13,24 @@ from accountapp.models import HelloWorld
 
 
 def hello_world(request):
-    if request.method == 'POST':
-        temp = request.POST.get('hello_world_input')
 
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
+    if request.user.is_authenticated:
 
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-    else:
+        if request.method == 'POST':
+            temp = request.POST.get('hello_world_input')
 
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+            new_hello_world = HelloWorld()
+            new_hello_world.text = temp
+            new_hello_world.save()
+
+            return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        else:
+
+            hello_world_list = HelloWorld.objects.all()
+            return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+    else :
+        return HttpResponseRedirect(reverse('accountapp:login'))
 
 
 class AccountCreateView(CreateView):
