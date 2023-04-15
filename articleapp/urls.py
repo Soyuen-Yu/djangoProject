d86a8e71f3ee397daef9b926388ede0699388989
soyuen <lseka@naver.com>
2023-04-15 14:58:29 +0900

    article의 create, detail 만들기

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -2,8 +2,12 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from django.views.generic import TemplateView
 
+from articleapp.views import ArticleCreateView, ArticleDetailView
+
 app_name = "articleapp" #appname을 명시하여 경로를 편리하게 설정할 수 있다.
 
 urlpatterns = [
     path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list'),
+    path('create/', ArticleCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
 ]
--- articleapp/views.py
+++ articleapp/views.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, DetailView
+
+from articleapp.forms import ArticleCreationForm
+from articleapp.models import Article
+
 
 # Create your views here.
+
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
+class ArticleCreateView(CreateView):
+    model = Article
+    form_class = ArticleCreationForm
+    template_name = 'articleapp/create.html'
+
+    def form_valid(self, form):
+        temp_article = form.save(commit=False)
+        temp_article.writer = self.request.user
+        temp_article.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk':self.object.pk})
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'articleapp/detail.html'
+    context_object_name = 'targetArticle'
